18ce3fab168cf8736db09ffdc866377b6d12b2d6
Alyssa Harvey <aharvey3869@gmail.com>
2016-03-20 10:57:23 -0400

    DTO_SPRT_GigCategories added.

---
+++ GigALoan_Model/DTO_SPRT_GigCategories.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace GigALoan_Model
+{
+    [DataContract]
+    public class DTO_SPRT_GigCategories
+    {
+        DTO_SPRT_GigCategories()
+        { }
+
+        DTO_SPRT_GigCategories(int id, string categoryName)
+        {
+            this.CategoryID = id;
+            this.CategoryName = categoryName;
+        }
+
+        [DataMember]
+        public int CategoryID { get; set; }
+        [DataMember]
+        public string CategoryName { get; set; }
+    }
+}
