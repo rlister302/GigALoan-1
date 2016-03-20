731f1d010cbf86b4101d48579466328faf66d0b0
Alyssa Harvey <aharvey3869@gmail.com>
2016-03-20 10:45:26 -0400

    DTO_CHLD_GigImages added.

---
+++ GigALoan_Model/DTO_CHLD_GigImages.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace GigALoan_Model
+{
+    [DataContract]
+    class DTO_CHLD_GigImages
+    {
+        DTO_CHLD_GigImages()
+        { }
+
+        DTO_CHLD_GigImages(int gigID, string imageName)
+        {
+            this.GigID = gigID;
+            this.ImageName = imageName;
+        }
+
+        [DataMember]
+        public int ImageID { get; set; }
+        [DataMember]
+        public int GigID { get; set; }
+        [DataMember]
+        public string ImageURL { get; set; }
+        [DataMember]
+        public string ImageUUID { get; set; }
+        [DataMember]
+        public string ImageName { get; set; }
+    }
+}
