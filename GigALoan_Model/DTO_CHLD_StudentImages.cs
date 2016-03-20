0e1d2e4935e441061f9a11e4ba9a67dfabd65f6d
Alyssa Harvey <aharvey3869@gmail.com>
2016-03-20 10:33:24 -0400

    DTO_CHLD_StudentImages added.

---
+++ GigALoan_Model/DTO_CHLD_StudentImages.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace GigALoan_Model
+{
+    [DataContract]
+    class DTO_CHLD_StudentImages
+    {
+        DTO_CHLD_StudentImages()
+        { }
+
+        DTO_CHLD_StudentImages(int studentID, string imageName)
+        {
+            this.StudentID = studentID;
+            this.ImageName = imageName;
+        }
+
+        [DataMember]
+        public int ImageID { get; set; }
+        [DataMember]
+        public int StudentID { get; set; }
+        [DataMember]
+        public string ImageURL { get; set; }
+        [DataMember]
+        public string ImageUUID { get; set; }
+        [DataMember]
+        public string ImageName { get; set; }
+
+    }
+}
