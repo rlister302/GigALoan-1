fdc52b681a6b8e460257be5c0b62fe2c6083ccc3
Alyssa Harvey <aharvey3869@gmail.com>
2016-03-20 10:38:56 -0400

    DTO_SPRT_Majors added.

---
+++ GigALoan_Model/DTO_SPRT_Majors.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace GigALoan_Model
+{
+    [DataContract]
+    class DTO_SPRT_Majors
+    {
+        DTO_SPRT_Majors()
+        { }
+
+        DTO_SPRT_Majors(int id, string name)
+        {
+            this.MajorID = id;
+            this.MajorName = name;
+        }
+
+        [DataMember]
+        public int MajorID { get; set; }
+        [DataMember]
+        public string MajorName { get; set; }
+    }
+}
