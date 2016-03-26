f24c9ee2c016950b953b20bc2f019b832293b9ac
Alyssa Harvey <aharvey3869@gmail.com>
2016-03-26 11:08:36 -0400

    Updated Model to reflect column additions in Student Table and new proc to GetLocalStudentsByAlert

--- GigALoan_DAL/Model1.Designer.cs
+++ GigALoan_DAL/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Nicolas\documents\visual studio 2013\Projects\GigALoan_Web\GigALoan_DAL\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Alyssa\Desktop\GigALoanFork\GigALoan_DAL\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
